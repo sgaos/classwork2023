8f86c610e62408848f7db6050c3ab67d62df6568
David Ward <david.a.ward@duke.edu>
2023-02-17 09:12:11 -0500

    Exception Handling Classwork and raising exceptions

--- my_math_calculator.py
+++ my_math_calculator.py
@@ -3,6 +3,13 @@ def sqrt(n):
     # We are using n itself as
     # initial approximation This
     # can definitely be improved
+    
+    if type(n) is str:
+        raise TypeError("Cannot send a string.")
+    if n < 0:
+        raise ValueError("You cannot send a negative number"
+                         " to this function.")
+             
 
     x = n
     y = 1

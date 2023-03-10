a41f0c92fa470b1457b4431657c32acafe313f32
David Ward <david.a.ward@duke.edu>
2023-03-09 21:20:15 -0500

    Added /add_test route to receive new test data and store with patient

--- health_db_client.py
+++ health_db_client.py
@@ -11,3 +11,8 @@ patient = {"mrn": 2, "name": "John", "blood_type": "A+"}
 r = requests.post(server + "/new_patient", json=patient)
 print(r.status_code)
 print(r.text)
+
+data = {"id": 1, "test_name": "HDL", "test_result": 150}
+r = requests.post(server + "/add_test", json=data)
+print(r.status_code)
+print(r.text)
--- health_db_server.py
+++ health_db_server.py
@@ -1,3 +1,8 @@
+# health_db_server.py
+
+from flask import Flask, request, jsonify
+
+
 """"
 Database = Dictionary
 keys -> ids for the patients
@@ -10,8 +15,6 @@ value: int
 
 """
 
-from flask import Flask, request, jsonify
-
 db = {}
 
 app = Flask(__name__)
@@ -19,8 +22,14 @@ app = Flask(__name__)
 def add_patient_to_db(id, name, blood_type):
     new_patient = {"id": id,
                    "name": name,
-                   "blood_type": blood_type }
+                   "blood_type": blood_type,
+                   "tests": []}
     db[id] = new_patient
+    print(db)
+
+
+def add_test_to_db(id, test_name, test_value):
+    db[id]["tests"].append((test_name, test_value))
     print(db)
 
 
@@ -55,6 +64,58 @@ def validate_input_data(in_data):
         if type(in_data[key]) is not value_type:
             return "Key {} has the incorrect value type".format(key)
     return True
+
+
+def validate_input_data_add_test(in_data):
+    if type(in_data) is not dict:
+        return "Input is not a dictionary"
+    expected_keys = ["id", "test_name", "test_result"]
+    expected_types = [int, str, int]
+    for key, value_type in zip(expected_keys, expected_types):
+        if key not in in_data:
+            return "Key {} is missing from input".format(key)
+        if type(in_data[key]) is not value_type:
+            return "Key {} has the incorrect value type".format(key)
+    return True
+
+
+def validate_input_data_generic(in_data, expected_keys, expected_types):
+    if type(in_data) is not dict:
+        return "Input is not a dictionary"
+    for key, value_type in zip(expected_keys, expected_types):
+        if key not in in_data:
+            return "Key {} is missing from input".format(key)
+        if type(in_data[key]) is not value_type:
+            return "Key {} has the incorrect value type".format(key)
+    return True
+
+
+@app.route("/add_test", methods=["POST"])
+def post_add_test():
+    in_data = request.get_json()
+    answer, status_code = add_test_driver(in_data)
+    return jsonify(answer), status_code
+
+
+def does_patient_exist_in_db(id):
+    if id in db:
+        return True
+    else:
+        return False
+
+def add_test_driver(in_data):
+    expected_keys = ["id", "test_name", "test_result"]
+    expected_types = [int, str, int]
+    validation = validate_input_data_generic(in_data, expected_keys, expected_types)
+    if validation is not True:
+        return validation, 400
+    does_id_exist = does_patient_exist_in_db(in_data["id"])
+    if does_id_exist is False:
+        return "Patient id {} does not exist in database"\
+            .format(in_data["id"]), 400
+    add_test_to_db(in_data["id"], in_data["test_name"],
+                   in_data["test_result"])
+    return "Test successfully added", 200
 
 
 if __name__ == '__main__':
